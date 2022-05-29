b5f044f5b49664c6511f6a988f07e797d7be04e7
Kanij Fatema <kfatema991@gmail.com>
2022-05-29 22:30:44 +0600

    singlePart-commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -24,6 +24,14 @@ async function run() {
             const parts = await cursor.toArray();
             res.send(parts);
         })
+
+        app.get('/singleParts/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const singleParts =await partCollection.findOne(query);
+            res.send(singleParts);
+        })
+
     }
     finally {
 
